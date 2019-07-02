1cb0bf127664c9e5027d52afc5154de96e1847e2
sylvanosullivan <sylvanosullivan@gmail.com>
2019-07-01 20:40:51 -0400

    back to getting json in the right format, need to streamline

--- model_run.py
+++ model_run.py
@@ -1,7 +1,4 @@
-from __future__ import unicode_literals, print_function
-import plac
 import random
-from pathlib import Path
 import spacy
 from spacy.util import minibatch, compounding
 from sklearn.model_selection import train_test_split
@@ -9,47 +6,61 @@ import pandas as pd
 import numpy as np
 
 
-output_dir = '.'
 
-datadir = '/Users/sylvan/Documents/python_projects/chatproj/chatproj/fitnessdata'
+def main():
+    output_dir = '.'
 
-
-df_text = pd.read_csv(datadir+'/train.txt',header=None)
-df_cats = pd.read_csv(datadir+'/labels.txt',header=None)
-df_cats.columns = ['is_fitness_app']
-df_text.columns=['description']
-cats = np.array(df_cats.is_fitness_app.apply(lambda y:{"POSITIVE": bool(y), "NEGATIVE": not bool(y)}))
+    datadir = 'fitnessdata/'
 
 
-xtrain,xtest,ytrain,ytest = train_test_split(df_text.description,df_cats.is_fitness_app)
+    df_text = pd.read_csv(datadir+'train.txt',header=None)
+    df_cats = pd.read_csv(datadir+'labels.txt',header=None)
+    df_cats.columns = ['is_fitness_app']
+    df_text.columns=['description']
+    cats = np.array(df_cats.is_fitness_app.apply(lambda y:{"POSITIVE": bool(y), "NEGATIVE": not bool(y)}))
 
 
-print("Loading from", output_dir)
-nlp2 = spacy.load(output_dir)
-
-testnlp = []
-for i in xtest:
-    testnlp.append(nlp2(i))
-# doc2 = nlp2(xtest)
+    xtrain,xtest,ytrain,ytest = train_test_split(df_text.description,df_cats.is_fitness_app)
 
 
-ri = ytest.reset_index()
+    print("Loading")
+    nlp2 = spacy.load(output_dir)
+
+    testnlp = []
+    for i in xtest:
+        testnlp.append(nlp2(i))
+    # doc2 = nlp2(xtest)
 
 
-
-accuracy = 0
-# ri.is_fitness_app[:30]
-for ind, el in enumerate(testnlp):
-    assessment = max(el.cats.keys(), key=(lambda k: el.cats[k]))
-    # el.cats,
-    actual = ri.is_fitness_app[ind]
-
-    if (assessment=='POSITIVE' and actual ==1):
-        accuracy +=1
-        
-    elif (assessment=='NEGATIVE' and actual ==0):
-        accuracy +=1
+    ri = ytest.reset_index()
 
 
 
-print(accuracy / len(testnlp))
+    accuracy = 0
+    # ri.is_fitness_app[:30]
+    for ind, el in enumerate(testnlp):
+        assessment = max(el.cats.keys(), key=(lambda k: el.cats[k]))
+        # el.cats,
+        actual = ri.is_fitness_app[ind]
+
+        if (assessment=='POSITIVE' and actual ==1):
+            accuracy +=1
+
+        elif (assessment=='NEGATIVE' and actual ==0):
+            accuracy +=1
+
+
+    print('categorization accuracy:')
+    print(accuracy / len(testnlp))
+
+    return testnlp
+
+def return_to_be_tagged(testnlp):
+    to_be_tagged = []
+    for ind, el in enumerate(testnlp):
+        assessment = max(el.cats.keys(), key=(lambda k: el.cats[k]))
+
+        if assessment=='POSITIVE':
+            to_be_tagged.append(el)
+
+    return to_be_tagged
